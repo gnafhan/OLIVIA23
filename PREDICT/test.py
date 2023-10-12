56b4af88d6a0de03a2cf1099c43b79da147607b1
gnafhan <nafhanghifari@gmail.com>
2023-10-12 16:52:55 +0700

    Add BERT

--- PREDICT/test.py
+++ PREDICT/test.py
@@ -1,9 +1,39 @@
 import pickle
-import sklearn
-# print(sklearn.__version__)
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model_nlp_pangan.pkl', 'rb'))
+from transformers import pipeline
 
-x = vectorizer.transform(['']).toarray()
-x = model.predict(x)
-print(x)
+pretrained_name = "w11wo/indonesian-roberta-base-sentiment-classifier"
+
+text = "kebijakan ini menyusahkan rakyat"
+
+label_index = {'0': 'negative', '1': 'neutral', '2': 'positive'}
+
+model_logistic = pickle.load(open('lr_pangan.pkl', 'rb'))
+
+pred_model_logistic = model_logistic.predict([text])
+
+pred_model_logistic = label_index[str(pred_model_logistic[0])]
+
+model_bert = pipeline(
+    "sentiment-analysis",
+    model=pretrained_name,
+    tokenizer=pretrained_name
+)
+
+pred_model_bert = model_bert.predict([text])
+
+pred_model_bert = pred_model_bert[0]['label']
+
+print("Logistic Regression: ", pred_model_logistic)
+print("BERT: ", pred_model_bert)
+
+'''
+Citation :
+@misc {wilson_wongso_2023,
+    author       = { {Wilson Wongso} },
+    title        = { indonesian-roberta-base-sentiment-classifier (Revision e402e46) },
+    year         = 2023,
+    url          = { https://huggingface.co/w11wo/indonesian-roberta-base-sentiment-classifier },
+    doi          = { 10.57967/hf/0644 },
+    publisher    = { Hugging Face }
+}
+'''
